81bf6c466ade4ff58e7ff6ae5e387ffe65f61b3f
mohnabil16 <moh.nabil63@gmail.com>
2023-03-09 21:27:54 +0700

    komit

---
+++ app/Http/Controllers/cstudentcontroller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cstudent;
+use Illuminate\Http\Request;
+
+class cstudentcontroller extends Controller
+{
+    //
+    function index()
+    {
+        $data = cstudent::all();
+        return $data;
+    }
+}
